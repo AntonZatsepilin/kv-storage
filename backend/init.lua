af73019c65dc4040fac11633299f3d81e91d5ab8
AntonZatsepilin <zatsepilin90.00@bk.ru>
2025-03-25 13:21:34 +0300

    Refactor Dockerfile and docker-compose configuration; update init.lua to create KV space and user permissions

--- backend/init.lua
+++ backend/init.lua
@@ -1,11 +1,17 @@
-box.cfg {
-    listen = 3301,
-    memtx_memory = 256
-}
-
 box.once("bootstrap", function()
-    -- Создаём пользователя guest, если его ещё нет
-    box.schema.user.create('guest', { if_not_exists = true })
-    -- Даём права на чтение, запись и выполнение на всю базу
-    box.schema.user.grant('guest', 'read,write,execute', 'universe', { if_not_exists = true })
+    box.schema.space.create('kv', {
+        if_not_exists = true,
+        format = {
+            {name = 'key', type = 'string'},
+            {name = 'value', type = '*'}
+        }
+    })
+    box.space.kv:create_index('primary', {
+        type = 'hash',
+        parts = {'key'},
+        if_not_exists = true
+    })
+    
+    box.schema.user.create('guest', {if_not_exists = true})
+    box.schema.user.grant('guest', 'read,write,execute', 'universe', {if_not_exists = true})
 end)
